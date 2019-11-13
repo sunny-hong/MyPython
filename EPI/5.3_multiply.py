d42f0cd12709bc3b0fd03d69ff095f7156662f5d
Sunny Hong <sunny@gatech.edu>
2019-11-13 13:17:36 -0800

    Added wrap to view question better

--- EPI/5.3_multiply.py
+++ EPI/5.3_multiply.py
@@ -1,4 +1,4 @@
-# Write a program that takes two arrays representing integers, and returns an integer representing their product. For example, since 193707721 * -761838257287 = -147573952589676412927 if the inputs are [1,9,3,7,0,7,7,2,1], your function should return the following array: [-1,4,7,5,7,3,9,5,2,5,8,9,6,7,6,4,1,2,9,2,7]. Hint - use arrays to simulate the grade-school multiplication algorithm. 
+# Write a program that takes two arrays representing integers, and returns an integer representing their product. For example, since 193707721 * -761838257287 = -147573952589676412927 if the inputs are [1,9,3,7,0,7,7,2,1], your function should return the following array: [-1,4,7,5,7,3,9,5,2,5,8,9,6,7,6,4,1,2,9,2,7]. Hint - use arrays to simulate the grade-school multiplication algorithm.   
 
 # Same as the book answer 
 
